0a04ebb816fdedb28110516642de82f5bab3cf22
meterscao <meterscao@gmail.com>
2022-11-17 11:55:24 +0800

    Add callback.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var callbackRouter = require('./routes/callback');
+
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -41,6 +43,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
+app.use('/callback', callbackRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
